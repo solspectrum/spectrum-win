d886acbb98d249e5389bb1d44f025de014dc89f5
/dev/fd0 <alicexbt@protonmail.com>
2022-04-10 18:44:16 +0530

    add files

---
+++ Account.xaml.cs
@@ -0,0 +1,121 @@
+﻿using QRCoder;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Spectrum
+{
+
+    public partial class Account : Window
+    {
+        StringBuilder transactions = new StringBuilder();
+        public Account()
+        {
+            InitializeComponent();
+            TxBox.Document.Blocks.Clear();
+            balance();            
+        }
+
+        private void OnTabSelected(object sender, RoutedEventArgs e)
+        {
+            var tab = sender as TabItem;
+            if (tab != null)
+            {
+                txhistory();
+            }
+        }
+
+        private void txhistory()
+        {
+            
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"\\1.9.11\\solana-release\\bin\\solana.exe",
+                    Arguments = "transaction-history tTfbP9xMQ3FXyuyFsK13eeTZp6ebX1FAY1YUNCzRvFr",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true
+
+                }
+            };
+
+            process.Start();
+
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+
+                if (line is not null)
+                {
+                    transactions.AppendLine(line);
+                }
+
+            }
+
+            TxBox.AppendText(transactions.ToString());
+        }
+
+        private void balance()
+        {
+            StringBuilder balance = new StringBuilder();
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"\\1.9.11\\solana-release\\bin\\solana.exe",
+                    Arguments = "balance tTfbP9xMQ3FXyuyFsK13eeTZp6ebX1FAY1YUNCzRvFr",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true
+
+                }
+            };
+
+            process.Start();
+
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+
+                if (line is not null)
+                {
+                    balance.AppendLine(line);
+                }
+
+            }
+
+            balance_label.Content= balance.ToString();
+        }
+
+        private void qrcode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(AddressList.SelectedItem.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);           
+
+            //TODO: Print QR Code for each address when selected in listbox
+        }
+
+        private void AddressList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            qrcode();
+        }
+    }
+
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows.Controls;
+
+namespace Spectrum
+{
+    public partial class MainWindow : Window
+    {
+        StringBuilder addresses = new StringBuilder();
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            seed_words.Document.Blocks.Clear();
+        }
+
+        private void DeriveAddresses_Click(object sender, RoutedEventArgs e)
+        {
+
+            Account Addresses = new Account();
+            Addresses.Show();
+            this.Close();
+
+            solanakeygen(1);            
+
+            Addresses.AddressList.Items.Clear();
+            Addresses.AddressList.Items.Add(addresses.ToString());
+
+            for (int i = 2;i< 9;i++)
+            {
+                addresses.Clear();
+                solanakeygen(i);
+                Addresses.AddressList.Items.Add(addresses.ToString());
+
+            }
+
+        }
+
+        string Seedbox(RichTextBox words)
+        {
+            TextRange textRange = new TextRange(
+                words.Document.ContentStart,
+
+                words.Document.ContentEnd
+            );
+            return textRange.Text;
+        }
+
+        private void solanakeygen(int index)
+        {
+            string words = Seedbox(seed_words);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = @"\\1.9.11\\solana-release\\bin\\solana-keygen.exe",
+                    Arguments = "pubkey prompt://?key=0/"+index,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardInput = true,
+                    CreateNoWindow = true
+
+                }
+            };
+
+            process.Start();
+
+            process.StandardInput.Write(words);
+            process.StandardInput.Write("\n");
+            process.StandardInput.Write("\n");            
+
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var line = process.StandardOutput.ReadLine();
+
+                if (line is not null)
+                {
+                    addresses.AppendLine(line);
+                }
+
+            }
+        }
+    }
+}
